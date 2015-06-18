2326e446ba0a7f9d34434761000634071f25d467
Francesco Camarlinghi <me@minifloppy.it>
2015-06-18 13:07:05 +0200

    Updated copyright notice

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Francesco Camarlinghi
+ * Copyright 2014-2015 Francesco Camarlinghi
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
