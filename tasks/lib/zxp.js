98c75cbeb4f5b3076ae059b4ec27924df9913a96
Alessandro Bianco <abianco@doxee.com>
2021-10-11 12:54:33 +0200

    Upgrade zxp-provider version

--- tasks/lib/zxp.js
+++ tasks/lib/zxp.js
@@ -26,7 +26,7 @@ module.exports = function (grunt)
     // Gets signing toolkit executable path
     var zxp_path = (function ()
     {
-        var exec_name = require('zxp-provider').bin;
+        var exec_name = require('zxp-provider')();
         // grunt.util.spawn will complain if exec is wrapped in quotes
         exec_name = exec_name.replace(/"(.*)"/, "$1");
         return exec_name;
